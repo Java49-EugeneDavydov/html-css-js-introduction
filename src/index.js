80e6e017e9e5b49b44614161b4df347975d71be3
DavydovEugene <eugene.kardinal@gmail.com>
2022-10-23 08:17:13 +0300

    HW #9 Landscape and Laptop designs with no detail's image

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ console.log("Launching script");
 const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
@@ -9,6 +11,13 @@ function setDetails(anchor) {
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         console.log("event - click on", anchors[i]);
+        ShowDetails();
         setDetails(anchors[i]);
     })
+}
+function ShowDetails() {
+    mainClass.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
 }
